a8347adff725320dbcbb0e5d4366f00793f9894a
rougeayrn <97561459+rougeayrn@users.noreply.github.com>
2022-09-02 18:37:40 -0400

    Silliness

--- src/utils/Utilities.ts
+++ src/utils/Utilities.ts
@@ -166,17 +166,6 @@ export class Utility {
         return `${h}${x}${v}${y}`
     }
 
-    /**
-     * A function that converts class properties to prototypes.
-     * @param base The class you wish to add prototypes to
-     * @param extra The class containing the prototypes
-     */
-    static extendClass(base: IPrototype, extra: IPrototype) {
-        let descs = Object.getOwnPropertyDescriptors(extra.prototype)
-        delete descs.prototype
-        Object.defineProperties(base.prototype, descs)
-    }
-
     static bodyCost(body: BodyPartConstant[]): number {
         if (!body || body.length == 0) return ERR_INVALID_ARGS;
         let cost = 0;
