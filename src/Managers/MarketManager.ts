907ab9fbeb7b1a8bcc2b3766e00473aa218b8d17
rougeayrn <97561459+rougeayrn@users.noreply.github.com>
2022-10-05 13:22:12 -0400

    Add files via upload

--- src/Managers/MarketManager.ts
+++ src/Managers/MarketManager.ts
@@ -9,6 +9,14 @@ export default class MarketManager {
         if (global.scheduler.processQueue.has(processName)) return;
 
         const task = () => {
+            const room = Game.rooms[roomName];
+            const terminal = room.terminal;
+            const storage = room.storage;
+            if (!terminal || !storage) return ProcessResult.RUNNING;
+
+            if (storage.store.getUsedCapacity(RESOURCE_ENERGY) > 450000 && terminal.store.getUsedCapacity(RESOURCE_ENERGY) > 20000) {
+                terminal.sell(RESOURCE_ENERGY, { quantity: terminal.store.energy - 20000 });
+            }
 
             return ProcessResult.RUNNING
         }
