8ace5f87498a665097f45cc8b388ec5926e738dc
rougeayrn <97561459+rougeayrn@users.noreply.github.com>
2022-10-26 17:23:34 -0400

    Add files via upload

--- src/Managers/RemoteManager.ts
+++ src/Managers/RemoteManager.ts
@@ -1,7 +1,10 @@
+import { MOVE_OPTS_CIVILIAN } from "Constants/MoveOptsConstants"
 import { CRITICAL } from "Constants/ProcessPriorityConstants"
 import { ProcessState, FATAL, RUNNING } from "Constants/ProcessStateConstants"
 import { Process } from "Models/Process"
 import { RoomStatistics } from "Models/RoomStatistics"
+import { generatePath, MoveTarget } from "screeps-cartographer"
+import { Utils } from "utils/Index"
 export default class RemoteManager {
     /**
      * Computes the maximum number of remotes to run.
@@ -20,7 +23,7 @@ export default class RemoteManager {
             if ((!room.memory ||
                 !room.memory.remoteSites ||
                 Object.keys(room.memory.remoteSites).length < this.allowedNumberOfRemotes) &&
-                Game.time % 750 == 0 &&
+                Game.time % 750 === 0 &&
                 Game.cpu.bucket > 150) {
                 this.setRemotes(room)
             }
@@ -32,6 +35,32 @@ export default class RemoteManager {
                 }
             }
 
+            // Run Creep Assignment Verification
+            if (Game.time % 1500 === 0) {
+                for (const remoteRoomName in room.memory.remoteSites) {
+                    let remoteDetails = room.memory.remoteSites[remoteRoomName];
+                    for (const id of [...remoteDetails.assignedEngIds]) {
+                        if (!Game.getObjectById(id)) remoteDetails.assignedEngIds.splice(remoteDetails.assignedEngIds.indexOf(id), 1);
+                    }
+
+                    for (const id of [...remoteDetails.assignedHarvIds]) {
+                        if (!Game.getObjectById(id)) remoteDetails.assignedHarvIds.splice(remoteDetails.assignedHarvIds.indexOf(id), 1);
+                    }
+
+                    for (const id of [...remoteDetails.assignedTruckerIds]) {
+                        if (!Game.getObjectById(id)) remoteDetails.assignedTruckerIds.splice(remoteDetails.assignedTruckerIds.indexOf(id), 1);
+                    }
+
+                    // TODO: Add carry requisite updating
+                    for (const key in remoteDetails) {
+                        if (['assignedHarvIds', 'assignedTruckerIds', 'assignedEngIds'].indexOf(key) >= 0) continue;
+                        let sourceDetails = remoteDetails[key as Id<Source>];
+                        delete sourceDetails.carryReq;
+                        console.log(`Considering ${remoteRoomName}: ${key}`)
+                        this.setSourceProperties(room, remoteRoomName, sourceDetails, sourceDetails.dist);
+                    }
+                }
+            }
             return RUNNING
         }
 
@@ -42,7 +71,7 @@ export default class RemoteManager {
     private static setRemotes(room: Room) {
         let roomsInMemory = Object.values(Memory.rooms).filter(
             x => x.intel &&
-                Game.map.getRoomLinearDistance(room.name, x.intel?.name ?? "") <= 3 &&
+                Game.map.getRoomLinearDistance(room.name, x.intel.name ?? "") <= 3 &&
                 x.intel && x.intel.sourceDetail &&
                 Object.keys(x.intel.sourceDetail).length < 3
         )
@@ -50,6 +79,7 @@ export default class RemoteManager {
         let roomFrontiers = room.memory.frontiers
         if (!roomFrontiers) { return }
 
+        // TODO: Convert to number[] where each number is path dist, then sort referencing the new array via find or indexOf. This will cheapen this sorting significantly.
         roomsInMemory.sort((a, b) => {
             if (!a.intel || !b.intel) { return 0 }
             let aPath = PathFinder.search(new RoomPosition(25, 25, room.name), { pos: new RoomPosition(25, 25, a.intel.name), range: 20 }).path
@@ -77,18 +107,60 @@ export default class RemoteManager {
 
         //TODO: Add a pass that checks our remotes as they stand, if their PathFinder.path(x -> y) is greater than 4 * 50 = 200 then remove them and do a final pass.
         //TODO: Alternatively, create a function that checks via Pathfinder to and never allows it to be greater than 200.
-        for (let remote of selectedRemotes) {
+        for (const remote of selectedRemotes) {
             if (!Cache.rooms[remote.name].remoteOf) Cache.rooms[remote.name].remoteOf = room.name;
             if (!room.memory.remoteSites) room.memory.remoteSites = {}
+
+            // Add dist and carryReq calculations.
+            let sourcesDetails = {...remote.sourceDetail};
+            for (const sourceDetails of Object.values(sourcesDetails)) {
+                this.setSourceProperties(room, remote.name, sourceDetails);
+            }
+
             if (!room.memory.remoteSites[remote.name]) {
                 room.memory.remoteSites[remote.name] = {
-                    ...remote.sourceDetail,
+                    ...sourcesDetails,
                     assignedHarvIds: [],
                     assignedTruckerIds: [],
                     assignedEngIds: [],
                 };
-                // TODO: Add dist and carryReq calculations.
             }
         }
+    }
+
+    private static setSourceProperties(room: Room, remoteRoomName: string, sourceDetails: SourceDetails, dist?: number) {
+        // Determine source energy per tick
+        let energyPerTick = 5;
+        if (Game.rooms[remoteRoomName]?.controller?.reservation) energyPerTick = 10;
+        if (Utils.Typeguards.isSourceKeeperRoom(remoteRoomName)) energyPerTick = 12;
+        console.log(`energyPerTick: ${energyPerTick}`)
+
+        if (!dist) {
+            // Determine start point
+            let start: RoomPosition | undefined;
+            if (room.storage) start = room.storage.pos;
+            else if (room.spawns[0]) start = room.spawns[0].pos;
+            else if (room.controller) start = room.controller.pos;
+            console.log(`Start: ${JSON.stringify(start)}`)
+            if (!start) return;
+
+            // Determine end point
+            let end: MoveTarget[] = [{ pos: Utils.Utility.unpackPostionToRoom(sourceDetails.packedPos, remoteRoomName), range: 1 }];
+            console.log(`End: ${JSON.stringify(end)}`)
+            if (!end) return;
+
+            // Set dist value
+            dist = generatePath(start, end, MOVE_OPTS_CIVILIAN)?.length
+            console.log(`Dist: ${dist}`)
+            if (!dist || dist <= 0) return;
+            if (dist && dist > 3) dist = dist - 2;
+            sourceDetails.dist = dist;
+        } else console.log(`Dist: ${dist}`)
+
+        // Calculate and set carryReq
+        let carryReq = Math.ceil(((dist * 2) * energyPerTick) / 50)
+        console.log(`CarryReq: ${carryReq}`)
+        if (!carryReq || carryReq <= 0) return;
+        sourceDetails.carryReq = carryReq;
     }
 }
