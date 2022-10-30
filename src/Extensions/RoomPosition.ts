ae4a73ee7f96075955ac7d469bb3729709dd6889
rougeayrn <97561459+rougeayrn@users.noreply.github.com>
2022-10-30 10:00:59 -0400

    Fixes

--- src/Extensions/RoomPosition.ts
+++ src/Extensions/RoomPosition.ts
@@ -7,6 +7,8 @@ declare global {
         onEdge: boolean;
         /** Determines if position is on or adjacent to the edge of the room. */
         nearEdge: boolean;
+        /** Determines if there are multiple adjacent positions open to the room position. */
+        multipleAdjacentOpen: boolean;
     }
 }
 
@@ -35,5 +37,32 @@ export default class RoomPosition_Extended extends RoomPosition {
             else this._nearEdge = false;
         }
         return this._nearEdge;
+    }
+
+    private _multipleAdjacentOpen: boolean | undefined;
+    get multipleAdjacentOpen(): boolean {
+        Utils.Logger.log("RoomPosition -> multipleAdjacentOpen", TRACE);
+        if (!this._multipleAdjacentOpen) {
+            let openCount = 0;
+            for (let x = -1; x <= 1; x++) {
+                for (let y = -1; x <= 1; x++) {
+                    const look = new RoomPosition(this.x + x, this.y + y, this.roomName).look();
+                    let open = true;
+                    for (const found of look) {
+                        // Creep
+                        if (found.type === LOOK_CREEPS || found.type === LOOK_POWER_CREEPS) open = false;
+                        // Terrain
+                        if (found.type === LOOK_TERRAIN && found.terrain === 'wall') open = false;
+                        // Structure
+                        if (found.type === LOOK_STRUCTURES && found.structure?.structureType !== STRUCTURE_ROAD && found.structure?.structureType !== STRUCTURE_CONTAINER) open = false;
+                    }
+                    if (open === true) openCount++;
+                }
+            }
+
+            if (openCount >= 2) this._multipleAdjacentOpen = true;
+            else this._multipleAdjacentOpen = false;
+        }
+        return this._multipleAdjacentOpen;
     }
 }
